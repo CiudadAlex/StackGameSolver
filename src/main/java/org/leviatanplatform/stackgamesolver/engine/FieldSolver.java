f2614a7d56bfd648b0323eb7e1d2dcaf1aefc378
CiudadAlex <ciudad.alex@gmail.com>
2026-04-11 00:43:41 +0200

    StackGameSolver

--- src/main/java/org/leviatanplatform/stackgamesolver/engine/FieldSolver.java
+++ src/main/java/org/leviatanplatform/stackgamesolver/engine/FieldSolver.java
@@ -3,25 +3,44 @@ package org.leviatanplatform.stackgamesolver.engine;
 import org.leviatanplatform.stackgamesolver.domain.Field;
 import org.leviatanplatform.stackgamesolver.domain.Move;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FieldSolver {
 
     public static List<Move> solve(Field field) {
 
-        List<Field> listNextFields = NextMoveCalculator.calculateNextPossibleMoves(field);
+        List<Field> listNextFields = List.of(field);
 
-        for (Field nextField : listNextFields) {
-            if (nextField.isSolved()) {
-                return nextField.getListMoves();
+        while (true) {
+
+            for (Field nextField : listNextFields) {
+                if (nextField.isSolved()) {
+                    return nextField.getListMoves();
+                }
             }
 
-            List<Move> listMoves = solve(nextField);
-            if (listMoves != null) {
-                return listMoves;
+            listNextFields = getNextFields(listNextFields);
+
+            System.out.print(".");
+        }
+    }
+
+    public static List<Field> getNextFields(List<Field> listFields) {
+
+        Map<String, Field> listAllNextFields = new HashMap<>();
+
+        for (Field field : listFields) {
+
+            List<Field> listNextFields = NextMoveCalculator.calculateNextPossibleMoves(field);
+
+            for (Field nextField : listNextFields) {
+                listAllNextFields.put(nextField.getPositionString(), nextField);
             }
         }
 
-        return null;
+        return new ArrayList<>(listAllNextFields.values());
     }
 }
