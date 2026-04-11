e9e8442eab8e9c2777d7cf1d0af5766d21f0910d
CiudadAlex <ciudad.alex@gmail.com>
2026-04-11 02:28:11 +0200

    StackGameSolver

--- src/main/java/org/leviatanplatform/stackgamesolver/Main.java
+++ src/main/java/org/leviatanplatform/stackgamesolver/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Field field = FieldColorExampleCreator.createMedium();
+        Field field = FieldColorExampleCreator.createHard();
 
         FieldColorVisualizer.visualize(field);
 
